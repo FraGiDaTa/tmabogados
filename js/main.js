af44d57cbaa06171d1c2df78fb883635f8a03c5b
CarloRojas <carlo2010pe@hotmail.com>
2022-09-14 02:06:58 -0500

    index

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+
+const app=document.getElementById('typewriter');
+
+
+const typewriter = new Typewriter(app, {
+    loop: true,
+    delay:120
+});
+
+typewriter
+.typeString('DERECHO PENAL')
+.pauseFor(800)
+.start();
+
+
+
